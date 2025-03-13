35c5faf1ba1b4abe7d44a963f36d11f71988085d
Breno Esteves <brenoesteves243@gmail.com>
2025-03-12 22:31:44 -0300

    🃏 Alterações na pagina principal e aplicando CSS na categoria SOBRE

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const logoBreno = document.getElementById("logo-breno"); // Seleciona a imagem dentro do #breno
+    const logoBreno = document.getElementById("logo-breno");
 
     logoBreno.addEventListener("mouseover", function () {
-        this.src = "medias/Logo colorida.png"; // Muda para a nova imagem
+        this.src = "medias/Logo colorida.png";
     });
 
     logoBreno.addEventListener("mouseout", function () {
-        this.src = "medias/Logo degrade.png"; // Volta para a imagem original
+        this.src = "medias/Logo degrade.png";
     });
 });
